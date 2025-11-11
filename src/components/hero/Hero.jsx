8aee47999f7081089abe19242ae8c1cb33ef37a4
Damian Sevilla <damian.sevilla@ram.local>
2025-11-11 12:27:02 -0600

    Changed hero structure and photos

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './hero.css'
 import building from '@assets/images/hero_cover.jpg'
-import services from '@assets/images/services.jpg'
-import manufacture from '@assets/images/manuf.jpg'
-import analisys from '@assets/images/analisys.jpg'
 import ford_logo from '@assets/images/clients/ford_logo.webp'
 import brp_logo from '@assets/images/clients/brp_logo.svg'
 import nemak_logo from '@assets/images/clients/nemak_logo.webp'
 import stellantis_logo from '@assets/images/clients/stellantis_logo.webp'
 import gm_logo from '@assets/images/clients/gm_logo.svg'
 import Products from '@components/home/products/Products'
+import about_us from '@assets/images//home/about_us.webp'
 
 const Hero = () => {
 
@@ -25,38 +23,23 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className="hero-content">
-                    <div className="card odd-card">
+                    <div className="card">
                         <div className="hero-card">
-                            <img src={services} alt="RAM" className='hero-card-img'/>
-                            <div className="hero-card-content">
+                            <div className="hero-card-title">
                                 <div className="section-pre-title">
                                     RAMOS ARIZPE MANUFACTURING
                                 </div>
                                 <div className="section-title">
                                     NUESTRA HISTORIA
                                 </div>
+                            </div>
+                            <div className="hero-card-content">
+                                <img src={about_us} alt="RAM" className='hero-card-img'/>
                                 <div className='hero-card-text font-roboto'>
                                     La empresa “RAM” es instituida legalmente a finales del segundo cuarto del 2009 como Powertrain Production Systems (“PPS”) a Royal Oak Industries. “PPS” ahora “RAM” fue establecida en el año 2005 quien abre operaciones con una Planta de 6700 metros cuadrados ubicada en Gorrión Lote 13 Manzana 3 Parque Industrial FINSA Ramos Arizpe, Coahuila C.P. 25904 México. <br /><br />
-                                    Ramos Arizpe Manufacturing es una de las plantas más grandes y con más experiencia de pre-maquinado y acabados de piezas para motores. 
+                                    Ramos Arizpe Manufacturing es una de las plantas más grandes y con más experiencia de pre-maquinado y acabados de piezas para motores.Nosotros estamos dedicados al maquinado de precisión de piezas de fundición con aleaciones de aluminio y hierro. Nuestra operación de clase mundial trabaja para múltiples OEM en América del Norte y del Sur, así como en Asia y Europa. 
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="hero-card">
-                            <div className="hero-card-content">
-                                <h2>Proceso de Producción</h2>
-                                <div className='hero-card-text'>
-                                    <ul>
-                                        <li>Estamos a la vanguardia en maquinado CNC</li>
-                                        <li>Verificación de fugas y sellado</li>
-                                        <li>Sistemas óptimos de producción</li>
-                                        <li>Certificación ISO 14001</li>
-                                        <li>Manufactura por lotes de alto y mediano volumen</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <img src={analisys} alt="RAM" className='hero-card-img'/>
                         </div>
                     </div>
                     <div className="card odd-card">
